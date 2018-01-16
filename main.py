5545709a8c728102753b678858d91e998a513d93
Ivan Borisenko <mteru00@gmail.com>
2018-01-17 00:29:06 +0300

    Minor fixes

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 from model.analizer import Analizer
 
 if __name__ == '__main__':
-    text = "Not clear deadline!"
+    text = '''What evidence can you share that demonstrates the impact of the engagement? 
+
+I’ve had multiple people comment positively on the clean branding, the site, and elements they’ve helped pull together. 
+
+How did DEKSIA perform from a project management standpoint? 
+
+I've worked with multiple agencies over the years, and DEKSIA are among the best communicators. We communicate through email and regular conference calls. Lauren is great about keeping in touch. If I have a question about my project, they respond very quickly, which is unusual for an agency. 
+
+What did you find most impressive about them? 
+
+They’re great at putting themselves in the client’s shoes and are fun to work with. You can tell Lauren is passionate about what he does. The whole team is very good at what they do and have many ideas to provide me. They’re always on their toes and see the project from my standpoint instead of just trying to make money and move on. They truly care about what they’re doing, and it shows throughout the whole process. 
+
+Are there any areas they could improve? 
+
+No. 
+
+Do you have any advice for potential customers? 
+
+The best thing you can do is fully communicate what you’re trying to do, whether it be your vision or its strategic implementations. Give them as much detail as possible. Once you do that, they will take it and run with it.
+второй
+Could you share any evidence that would demonstrate the productivity, quality of work, or the impact of the engagement? 
+
+Quantitatively we don't have any metrics, but qualitatively we've gotten great feedback from people who see our name and now see the connection between our name and our organization. 
+
+How did Deksia perform from a project management standpoint? 
+
+They are excellent at project management. Aaron is the main guy who serves as their project manager, and he is always available by phone, email, or text, and will respond to you within the hour. Corey was another team member who we worked with directly, and he was also incredibly helpful. 
+
+What did you find most impressive about Deksia? 
+
+What sets them apart is Aaron's deep knowledge of our business. What our company does is not something that an everyday person has common knowledge of, for instance running a restaurant or a similar business, but Aaron takes the time to really understand us and the intricacies of our company which we really appreciate and had led us to a successful outcome with our new name and branding campaign. 
+
+Are there any areas Deksia could improve? 
+
+I don't think there's anything they can improve upon at this point.'''
     Analizer.process_text(text)
 
--- model/analizer.py
+++ model/analizer.py
@@ -1,11 +1,16 @@
+import logging
+
 from data import *
 from .reports.keyword import KeywordReport
 
+
+logging.basicConfig(filename='analizer.log',level=logging.DEBUG, filemode='w')
 
 class Analizer:
     @staticmethod
     def process_text(text):
         """ Process text. """
+        logging.debug("Starting text processing...")
         normalized_text = Analizer.__normalize_text(text)
         sentences = Analizer.__get_sentences(normalized_text)
         positive, negative = 0, 0
@@ -20,16 +25,18 @@ class Analizer:
     @staticmethod
     def process_sentence(sentence):
         """ Process sentence. """
+        logging.debug("Starting sentence processing for '{0}'".format(sentence))
         results = []
         words = Analizer.__get_words(sentence)
-        for index, word in enumerate(words):
-            for aspect_category in ASPECT_CATEGORIES:
-                for keyword in ASPECT_CATEGORIES[aspect_category]:
-                    if keyword in sentence:
-                        report = Analizer.__process_context(words, word, index)
-                        if report:
-                            report.category = aspect_category
-                            results.append(report)
+        for aspect_category in ASPECT_CATEGORIES:
+            for keyword in ASPECT_CATEGORIES[aspect_category]:
+                if keyword in sentence:
+                    logging.debug("Found keyword '{0}' in '{1}'".format(keyword, sentence))
+                    report = Analizer.__process_context(words, keyword.split()[0])
+                    if report:
+                        logging.debug("Report created for '{0}'".format(sentence))
+                        report.category = aspect_category
+                        results.append(report)
         return results
 
     @staticmethod
@@ -51,13 +58,23 @@ class Analizer:
     @staticmethod
     def __get_words(sentence):
         """ Returns list of words from given sentence. """
-        return sentence.split()
+        logging.debug("Getting words for '{0}'".format(sentence))
+        words = sentence.split()
+        for index, _ in enumerate(words):
+            words[index] = words[index].replace('!', '')
+            words[index] = words[index].replace('?', '')
+            words[index] = words[index].replace('.', '')
+        logging.debug("Words returned: {0}".format(words))
+        return words
 
     @staticmethod
-    def __process_context(sentence, word, index):
+    def __process_context(sentence, word, index=None):
+        logging.debug("Starting context processing for <{0}>".format(sentence))
+        index = sentence.index(word) if not index else index
         context = Analizer.__get_context(sentence, index)
         evaluation, evaluation_rate = Analizer.__get_evaluation(context)
         if evaluation:
+            logging.debug("Evaluation word found: '{0}'".format(sentence))
             intensifier, intensifier_rate = Analizer.__get_intensifier(context)
             inverter_word, success = Analizer.__get_inverter(context)
             positive, negative = Analizer.__get_keyword_summary(
