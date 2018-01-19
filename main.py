e5549b0345695d852bf9507a9e5f070e556a1e6a
Ivan Borisenko <mteru00@gmail.com>
2018-01-19 17:29:40 +0300

    Minor changes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
+
 from model.analizer import Analizer
 from view.terminal.printer import TerminalPrinter
 
  
 if __name__ == '__main__':
-    text = '''They’re very reliable. At times, we’ve challenged them with outside-the-box projects, and they’ve always performed at a high level. That includes communicating professionally, being transparent, and hitting deadlines.'''
+    text = input("Enter text to process: ")
     result = Analizer.process_text(text)
     TerminalPrinter.out(result)
-
+    os.system("pause")
